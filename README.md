19ec35727cd0e975dee5a9029c87da2d6d5e65c2
Ruben E. Acosta <eldragondera1@gmail.com>
2018-12-15 21:31:40 -0500

    template engine custom

--- README.md
+++ README.md
@@ -4,5 +4,17 @@ npm i express-generator -g
 
 express --view=pug {name-directory-app}
 
+```
 DEBUG=commerce:* npm start
 NODE_ENV=production npm starts
+```
+
+## Template Engine
+
+app.engine('ext', function(filePath, options, callback) {})
+
+app.set('views', './views')  --> Specify the views directory
+
+app.set('view engine', 'ext')  --> Register the template engine
+
+
---
+++ template_engine/index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const expressJsx = require('./express-jsx')
+
+// Register engine
+app.engine('jsx', expressJsx)
+
+// select engine
+app.set('view', './views')
+app.set('view engine', 'jsx')
+
+app.get('/', (req, res) => {
+  res.render('index', {Hello: 'Hola', World: 'Mundo'})
+})
+
+const PORT = 8000
+const server = app.listen(PORT, () => {
+  console.log(`Listenin http://localhost:${server.address().port}`)
+})
