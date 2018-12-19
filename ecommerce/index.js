138725d9e4e7f84c1f0e7572868667e1fef35a5b
Ruben E. Acosta <eldragondera1@gmail.com>
2018-12-18 21:03:53 -0500

    agragando bulma

--- ecommerce/index.js
+++ ecommerce/index.js
@@ -3,11 +3,13 @@ const app = express()
 const path = require('path')
 const productsRouter = require('./routes/products')
 
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
 // establecemos endpoints
 app.use('/products', productsRouter )
+app.use('/static', express.static(path.join(__dirname, 'public')))
 
 
 const PORT = 8000
--- ecommerce/routes/products.js
+++ ecommerce/routes/products.js
@@ -1,19 +1,21 @@
 const express = require('express')
 const router = express.Router()
 
-const products = [
+const myProducts = [
   {
     name: 'Pan',
-    price: 1
+    price: 1,
+    image: 'http://www.recetasdethermomix.es/wp-content/uploads/2013/02/pan-naranja-confitada-thermomix-400x300.jpg'
   },
   {
     name: 'Leche',
-    price: 3
+    price: 3,
+    image: 'http://apsal.org/wp-content/uploads/2016/12/Leche_de_vaca.jpg'
   }
 ]
 
 router.get('/', (req, res) => {
-  res.render('products', {products})
+  res.render('products', {myProducts})
 })
 
 module.exports = router
