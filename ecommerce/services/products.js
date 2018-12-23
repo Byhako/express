d79555171aed47f17cc894e40c36ac0b40c35441
Ruben E. Acosta <eldragondera1@gmail.com>
2018-12-23 01:35:09 -0500

    acciones CRUD en MongoDB

--- ecommerce/services/products.js
+++ ecommerce/services/products.js
@@ -16,28 +16,37 @@ class ProductsService {
   }
 
 
-  getProduct ({productId}) {
-    return Promise.resolve(productsMocks[0])
+  async getProduct({ productId }) {
+    const product = await this.mongoDB.get(this.collection, productId)
+    return product || {}
   }
 
 
-  createProduct ({product}) {
-    return Promise.resolve(productsMocks[0])
+  async createProduct({ product }) {
+    const createProductId = await this.mongoDB.create(this.collection, product)
+
+    return createProductId
   }
 
 
-  updateProduct ({productId, product}) {
-    return Promise.resolve(productsMocks[0])
+  async updateProduct({ productId, product }) {
+    const updateProductId = await this.mongoDB.update(
+      this.collection,
+      productId,
+      product
+    )
+
+    return updateProductId
   }
 
 
-  modifyProduct ({productId, product}) {
-    return Promise.resolve(productsMocks[0])
-  }
+  async deleteProduct({ productId }) {
+    const deletedProductId = await this.mongoDB.delete(
+      this.collection,
+      productId
+    )
 
-
-  deleteProduct ({productId}) {
-    return Promise.resolve(productsMocks[0])
+    return deletedProductId
   }
 }
 
